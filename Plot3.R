41c237968961be00c46bf74211c9cd08809df076
Arun Keskar <abkeskar4411@gmail.com>
2026-06-16 19:40:16 +0530

    Add plot3.R to visualize energy sub metering

---
+++ Plot3.R
@@ -0,0 +1,11 @@
+png("plot3.png", width=480, height=480)
+plot(plot_data$DateTime, plot_data$Sub_metering_1, 
+     type="l", 
+     xlab="", 
+     ylab="Energy sub metering")
+lines(plot_data$DateTime, plot_data$Sub_metering_2, col="red")
+lines(plot_data$DateTime, plot_data$Sub_metering_3, col="blue")
+legend("topright", 
+       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       col=c("black", "red", "blue"), lty=1)
+dev.off()
