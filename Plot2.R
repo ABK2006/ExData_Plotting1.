3406aed8ef2b1cf38d7dcf76a675954ef4b6a02a
Arun Keskar <abkeskar4411@gmail.com>
2026-06-20 23:00:43 +0530

    Fix plot function to use correct variables

--- Plot2.R
+++ Plot2.R
@@ -1,6 +1,13 @@
+# 1. Read and subset the data
+dataFile <- "household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".", na.strings="?")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+# 2. Convert Date and Time variables to a single Date/Time class
+datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+
+# 3. Open the PNG device, create the plot, and close the device
 png("plot2.png", width=480, height=480)
-plot(plot_data$DateTime, plot_data$Global_active_power, 
-     type="l", 
-     xlab="", 
-     ylab="Global Active Power (kilowatts)")
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 dev.off()
