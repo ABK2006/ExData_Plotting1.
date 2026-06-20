435b1193adf826b207bef38b979df6f784a37bf9
Arun Keskar <abkeskar4411@gmail.com>
2026-06-20 22:59:05 +0530

    Fix histogram plotting for global active power

--- Plot1.R
+++ Plot1.R
@@ -1,6 +1,15 @@
+# 1. Read the full dataset
+# Note: missing values are coded as '?'
+dataFile <- "household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".", na.strings="?")
+
+# 2. Subset the data for the required dates
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+# 3. Extract the required variable
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+
+# 4. Open the PNG device, create the plot, and close the device
 png("plot1.png", width=480, height=480)
-hist(plot_data$Global_active_power, 
-     col="red", 
-     main="Global Active Power", 
-     xlab="Global Active Power (kilowatts)")
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
 dev.off()
